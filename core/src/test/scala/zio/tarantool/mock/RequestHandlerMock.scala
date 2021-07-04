45cac7118bcf23f99ffd6da42e76f44c61f711b9
Nikita Ryanov <ryanov.nikita@gmail.com>
2021-07-04 20:16:30 +0300

    Update specs

--- core/src/test/scala/zio/tarantool/mock/RequestHandlerMock.scala
+++
@@ -1,7 +0,0 @@
-package zio.tarantool.mock
-
-import zio.tarantool.core.RequestHandler
-import zio.test.mock.mockable
-
-@mockable[RequestHandler.Service]
-object RequestHandlerMock
--- core/src/test/scala/zio/tarantool/mock/ResponseHandlerMock.scala
+++
@@ -1,7 +0,0 @@
-package zio.tarantool.mock
-
-import zio.tarantool.core._
-import zio.test.mock.mockable
-
-@mockable[ResponseHandler.Service]
-object ResponseHandlerMock
