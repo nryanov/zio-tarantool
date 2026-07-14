ba04f352d13add1837ab5f082c1dbb4f38d8cd05
Nikita Ryanov <ryanov.nikita@gmail.com>
2026-07-14 20:16:22 +0300

    test: fix flaky test

--- core/src/test/scala/zio/tarantool/TarantoolBaseSpec.scala
+++ core/src/test/scala/zio/tarantool/TarantoolBaseSpec.scala
@@ -24,14 +24,13 @@ trait TarantoolBaseSpec extends DefaultRunnableSpec with BaseLayers {
   def awaitResponseData[A: TupleEncoder](operation: Promise[TarantoolError, TarantoolResponse]) =
     awaitResponse(operation).flatMap(_.resultSet[A])
 
-  def createSpace(): ZIO[TarantoolClient, Throwable, Unit] = for {
-    r1 <- TarantoolClient.eval("box.schema.create_space('test', {if_not_exists = true})")
-    r2 <- TarantoolClient.eval(
-      "box.space.test:create_index('primary', {if_not_exists = true, unique = true, parts = {1, 'string'} })"
-    )
-    _ <- r1.await
-    _ <- r2.await
-  } yield ()
+  def createSpace(): ZIO[TarantoolClient, Throwable, Unit] =
+    TarantoolClient
+      .eval("""
+        box.schema.create_space('test', {if_not_exists = true})
+        box.space.test:create_index('primary', {if_not_exists = true, unique = true, parts = {1, 'string'}})
+      """)
+      .flatMap(_.await.unit)
 
   def createFunction() = for {
     r1 <- TarantoolClient.eval(
@@ -44,5 +43,5 @@ trait TarantoolBaseSpec extends DefaultRunnableSpec with BaseLayers {
     TarantoolClient.eval("return box.space.test.id").flatMap(_.await.flatMap(_.head[Int]))
 
   def truncateSpace(): ZIO[TarantoolClient, Throwable, Unit] =
-    TarantoolClient.eval("box.space.test:truncate()").flatMap(_.await.unit)
+    TarantoolClient.eval("if box.space.test then box.space.test:truncate() end").flatMap(_.await.unit)
 }
